5cb8d052f95c3446151c4a36ba39bb5094d2a049
Cloud User <centos@ip-172-31-26-94.us-west-2.compute.internal>
2017-06-15 02:48:42 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,27 @@
+package 'nano' do
+	action :install
+end
+
+package 'vim-enhanced' do
+	action :install
+end
+
+package 'ntp' do
+	action :install
+end
+
+package 'tree' do
+	action :install
+end
+
+file '/etc/motd' do
+	content 'this server is owned by tinman'
+	action :create
+	owner 'root'
+	group 'root'
+	mode '0655'
+end
+
+package 'git' do
+	action :install
+end
