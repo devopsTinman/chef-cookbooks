e44b399dab05cc592ac4375ab1f6d3656a53e785
Tinman 487 <devops.tinman@gmail.com>
2017-06-23 18:41:14 +0000

    updated motd with server details

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,10 +2,6 @@ package 'nano' do
 	action :install
 end
 
-package 'emacs' do
-	action :install
-end
-
 package 'vim-enhanced' do
 	action :install
 end
@@ -18,8 +14,20 @@ package 'tree' do
 	action :install
 end
 
+# node['ipaddress']
+# node['memory']['total']
+
+# print statement 'I have 4 apples'
+# apple_count = 4
+# puts "I have #{apple_count} apples"
+
 file '/etc/motd' do
-	content 'this server is owned by somebody'
+	content "this server is owned by somebody
+	Hostname: #{node['hostname']}
+	IP Address: #{node['ipaddress']}
+	CPU: #{node['cpu']['0']['mhz']}
+	Memory: #{node['memory']['total']}
+	"
 	action :create
 	owner 'root'
 	group 'root'
