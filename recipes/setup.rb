c9a04fc63e2c247ff36e57aaffbd0200c10c9dca
Tinman 487 <devops.tinman@gmail.com>
2017-07-07 16:04:17 +0000

    added user1 addition

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -35,3 +35,15 @@ end
 package 'git' do
 	action :install
 end
+
+user 'user1' do
+	comment 'user1'
+	uid '123'
+	home '/home/user1'
+	shell '/bin/bash'
+end
+
+group 'admins' do
+	members ['user1']
+	append true
+end
